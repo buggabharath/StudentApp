2f81f0743600b6c49f422a33c42594ca725dabf5
bob <bob@tutorial.com>
2017-07-22 11:18:28 +0530

    new project is added

---
+++ StudentApp/StudentApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+        public string StudName { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+        public string Standard { get; set; }
+    }
+}
